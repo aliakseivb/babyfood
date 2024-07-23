8a006ed6bb709aff7f518b81f100353127905920
AlexBoronin <alex.boronin.22@gmail.com>
2024-07-23 17:12:49 +0300

    new project

---
+++ script.js
@@ -0,0 +1,26 @@
+(function () {
+  /** бургер меню и меню навигации*/
+  const body = document.querySelector('body');
+  const nav = document.querySelector('.nav');
+  const burger = document.querySelector('.burger');
+  const navMenu = document.querySelector('.nav-menu');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('show');
+    body.classList.toggle('hidden');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (nav.classList.contains('show')&& e.target === nav) {
+      nav.classList.remove('show');
+      burger.classList.remove('active');
+    }
+    if (e.target.classList.contains('nav__link')) {
+      nav.classList.remove('show');
+      burger.classList.remove('active');
+      body.classList.remove('hidden');
+    }
+  });
+
+}());
